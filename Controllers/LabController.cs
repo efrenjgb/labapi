8a158e22b5a0c3a86dc0a46f47f6cb43f181ceeb
Efren Gonzalez <efrenjgb@gmail.com>
2021-08-28 21:58:16 -0400

    Initial commit

---
+++ Controllers/LabController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace labapi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<object>> Get()
+        {
+            //reemplazar SERVERNAME, DATABASENAME, USERNAME, PASSWORD con los que tiene el SqlSever que se va usar
+            using var conn = new SqlConnection("Server=SERVERNAME;Database=DATABASENAME;user id=USERNAME;password=PASSWORD;MultipleActiveResultSets=true");
+
+            //este es un query de ejemplo
+            var data = await conn.QueryAsync("select * from patients");
+
+            return data;
+        }
+    }
+}
